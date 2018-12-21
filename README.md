b1667d6af9f85a033ab613323a421e2041c837fc
Duncan Marsh <slugbyte@slugbyte.com>
2018-12-21 22:45:06 +0800

    uploadStatic and deleteStale functions working

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 A two function lib for uploading static websites and removing stale files for AWS S3.
 
 ## uploadStatic
+`uploadStatic` will recursivly upload a directorys contents to an AWS S3 Bucket.
 TODO
 ## deleteStale
-TODO
+`deleteStale` will diff a directory with and S3 bucket and delete files from the s3 bucket that dont exist localy. It can be used to to remove stale files with hashed names.
+TOD
---
+++ index.js
@@ -0,0 +1,123 @@
+const fs = require('fs')
+const path = require('path')
+const klaw = require('klaw')
+const util = require('util')
+const {S3} = require('aws-sdk')
+const {Readable} = require('stream')
+const through2 = require('through2')
+const s3 = new S3()
+
+if(!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY){
+  console.error('ERROR: AWS ENV CREDENTIALS NOT SET')
+  console.error('https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-environment.html')
+  process.exit(1)
+}
+
+const streamToPromise = (stream) => new Promise((resolve, reject) => {
+  stream.on("end", resolve(stream.dests[0].path));
+  stream.on("error", reject);
+})
+
+const uploadFile = (config) => s3.upload({
+  ACL: config.acl,
+  Bucket: config.bucket,
+  Key: path.relative(config.dir, config.filepath),
+  Body: fs.createReadStream(config.filepath),
+}).promise()
+
+let transform = (cb) => {
+  return through2.obj((f, _, next) => {
+    cb(f, (err, data) => {
+      if(err) return next(err)
+      if(data) return next(null, data)
+      next()
+    })
+  })
+}
+
+module.exports = {
+  uploadStatic: (config) => {
+    if(!config.dir)
+      throw new Error('ERROR: no dir supplied')
+    if(!config.bucket) 
+      throw new Error('ERROR: no bucket name supplied')
+
+    return klaw(config.dir)
+    .pipe(transform((f, cb) => {
+      // PASS FILES ON NO OTHER TYPES (DIR, PIPE, ECT)
+      fs.lstat(f.path, (err, data) => {
+        if(err) return cb(err)
+        if(data.isFile()) return cb(null, f.path)
+        cb()
+      })
+    }))
+    .pipe(transform((filepath, cb) => {
+      // UPLOAD TO S3
+      s3.upload({
+        ACL: config.acl,
+        Bucket: config.bucket,
+        Body: fs.createReadStream(filepath),
+        Key: path.relative(config.dir, filepath),
+      }, (err, data) => {
+        if(err) return cb(err)
+        if(data) return cb(null, {filepath, s3Object: data})
+        return cb(new Error('UNKNOWN ERROR: failed to upload ' + filepath))
+      })
+    }))
+  }, 
+  deleteStale: (config) => {
+    // buffer local files
+    // buffer s3 objects
+    // use filter to find "stale" objects on s3 that arenot in local dir
+    // delete s3 obejcts
+    
+    if(!config.dir)
+      throw new Error('ERROR: no dir supplied')
+    if(!config.bucket) 
+      throw new Error('ERROR: no bucket name supplied')
+    function DeleteStaleStream(){
+      Readable.call(this, {objectMode: true})
+      this.start = true
+    }
+    util.inherits(DeleteStaleStream, Readable)
+    DeleteStaleStream.prototype._read = function(){
+      if(this.start){
+        this.start = false
+        let filepaths = {}
+        // buffer the files in the local dir
+        klaw(config.dir)
+        .on('error', () => this.emit('error', err))
+        .on('data', (f) => {
+          // PASS FILES ON NO OTHER TYPES (DIR, PIPE, ECT)
+          fs.lstat(f.path, (err, data) => {
+            if(err) return this.emit('error', err)
+            if(data.isFile()){
+              filepaths[path.relative(config.dir, f.path)] = f
+            }
+          })
+        })
+        .on('end', () => {
+          // buffer the s3Objects from s3
+          s3.listObjectsV2({Bucket: config.bucket}, (err, data) => {
+            if(err) return this.emit('error', err)
+            // find and push to streram s3Objects that are not in the local dir
+            data.Contents.filter(i => !filepaths[i.Key])
+            .forEach(s3Object => this.push(s3Object))
+            this.push(null)
+          })
+        })
+      }
+    }
+    return new DeleteStaleStream()
+    .pipe(transform((s3Object, cb) => {
+      // DELETE FROM S3
+      s3.deleteObject({
+        Key: s3Object.Key,
+        Bucket: config.bucket,
+      }, (err, data) => {
+        if(err) return cb(err)
+        cb(null, data)
+      })
+    }))
+  },
+}
